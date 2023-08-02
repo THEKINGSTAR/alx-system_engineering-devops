6b4274e26365be031b7b409b444d34527377dabf
THEKINGSTAR <thekingstar5002@gmail.com>
2023-08-02 13:59:47 +0300

    Positive lookbehind assertion in TASK # 8. Textme

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,3 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\bfrom:([^\]]+)\]\s*\[to:(\+\d+)\]\s*\[flags:(.+?)\]/).join(',')
+#puts ARGV[0].scan(/\bfrom:([^\]]+)\]\s*\[to:(\+\d+)\]\s*\[flags:(.+?)\]/).join(',')
+puts ARGV[0].scan(/(?<=from:|to:|flags:).+?(?=\])/).join(',')
