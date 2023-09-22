df31169d231413541c851a9d7299995bab90d122
THEKINGSTAR <thekingstar5002@gmail.com>
2023-09-23 01:05:22 +0300

    1. Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 # Using Puppet, install flask from pip3.
 
 exec {"ISNTALL FLASK FROM PIP3":
-command  => 'pip3 install flask 2.1.0',
+command  => 'pip3 install flask==2.1.0',
 provider => shell,
 }
