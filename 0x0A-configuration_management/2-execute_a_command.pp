e556b21e64b6c4892ecb1e189985407ac0766b4e
THEKINGSTAR <thekingstar5002@gmail.com>
2023-09-23 04:21:24 +0300

    puppet-lint TASK # 2

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 # Using Puppet, create a manifest that kills a process named killmenow.
 
-exec {"killmenow":
+exec {'killmenow':
         command  => 'pkill -x killmenow',
         provider => shell,
         path     => '/usr/local/bin:/usr/bin'
