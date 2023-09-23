30d3c268aae55770fb376bd86614945128313395
THEKINGSTAR <thekingstar5002@gmail.com>
2023-09-23 04:20:36 +0300

    puppet-lint TAKS #1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,13 +1,13 @@
 # Using Puppet, install flask from pip3.
 
 package { 'python3-pip':
-    ensure   => present
-  }
+  ensure => present,
+}
 
-exec {"Flask":
-        command  => 'pip3 install flask==2.1.0',
-        provider => shell,
-	path     => '/usr/local/bin:/usr/bin',
-        unless   => 'pip3 show Flask | grep Version | grep -q 2.1.0',
-        require  => Package['python3-pip']
+exec { 'Flask':
+  command  => 'pip3 install flask==2.1.0',
+  provider => shell,
+  path     => '/usr/local/bin:/usr/bin',
+  unless   => 'pip3 show Flask | grep Version | grep -q 2.1.0',
+  require  => Package['python3-pip'],
 }
