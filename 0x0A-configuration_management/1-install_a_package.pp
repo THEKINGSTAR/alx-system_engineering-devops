86c8bca9086cab9250f2a6b905e46db5848b4874
THEKINGSTAR <thekingstar5002@gmail.com>
2023-09-23 04:00:44 +0300

    1. Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,14 @@
 # Using Puppet, install flask from pip3.
 
 package { 'python3-pip':
-  ensure => installed,
-}
+    ensure   => present,
+    provider => 'pip3',
+  }
+
 exec {"Flask":
-	command  => 'pip3 install flask==2.1.0',
-	provider => shell,
+        command  => 'pip3 install flask==2.1.0',
+        provider => shell,
 	path     => '/usr/local/bin:/usr/bin',
-	unless   => 'pip3 show Flask | grep Version | grep -q 2.1.0',
-	require  => Package['python3-pip']
+        unless   => 'pip3 show Flask | grep Version | grep -q 2.1.0',
+        require  => Package['python3-pip']
 }
