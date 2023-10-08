cd321e8cfaa27f7e45b1e550f98cfd1a075a9ced
THEKINGSTAR <thekingstar5002@gmail.com>
2023-10-08 21:32:27 +0300

    correct the output of the file in task #1

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,13 @@
 #4. Client configuration file (w/ Puppet)
+
 execute_code{'Turn off passwd auth':
-	ensure => 'present',
-	path   => '/etc/ssh/ssh_config',
-	line   => 'PasswordAuthentication no',
-	match  => 'PasswordAuthentication yes',
+ensure	=> 'present',
+path	=> '/etc/ssh/ssh_config',
+line	=> 'PasswordAuthentication no',
+match	=> 'PasswordAuthentication yes',
 }
 execute_code{'Turn off passwd auth':
-        ensure => 'present',
-        path   => '/etc/ssh/ssh_config',
-        line   => 'IdentityFile ~/.ssh/school'
+ensure 	=> 'present',
+path   	=> '/etc/ssh/ssh_config',
+line   	=> 'IdentityFile ~/.ssh/school'
 }
